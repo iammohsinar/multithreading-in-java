25adaced3321af3cd484837e646b6ff63707e547
mohsin.azeem <mohsin.azeem@vodafone.qa>
2025-10-27 14:12:20 +0500

    Added bank account simulation app

--- readme.md
+++ readme.md
@@ -39,7 +39,29 @@ NEW → RUNNABLE → RUNNING → BLOCKED → TERMINATED
 ```sleep()```, ```join()```, ```yield()```, ```setPriority()```, ```getPriority()```,
 ```isAlive()```, ```getName()```, ```setName()```
 
+- ```join()``` 
+  - You have 3 threads:
+    Imagine we have three threads: T1, T2, and T3. Now, suppose T1 calls `T2.join()`. This means that T1 will pause its execution and wait until T2 finishes its work. While T1 is waiting, T2 continues to run normally, and T3 remains completely independent, continuing its work without any interruption. Once T2 finishes execution, the `join()` call in T1 unblocks, allowing T1 to resume from where it left off. This mechanism is often used when one thread needs to wait for another to complete before proceeding.
+
+
+- ```yeild()```
+  - It tells the CPU scheduler it’s willing to pause. The thread goes from Running → Runnable (ready state).  The scheduler may pick another thread of equal or higher priority to run next. Or, it might immediately pick the same thread again because it’s only a suggestion, not a command. The CPU scheduler may or may not listen to it. — so sometimes it looks like nothing happened. 
+
+- ```sleep(ms)```
+  - Actually pauses for a fixed time.
+ 
+
 ## In ```miniproject``` package:
 
 - Parallel Sum Calculator use of ```ExecutorService```,```Callable```, ```Future```
-
+- Bank Account Simulation use of
+  - `Lock`, `Reentrantlock`,`AtomicInteger`
+  - Multiple threads deposit and withdraw safely. 
+  - To check the difference in **output** uncomment the code from line `25` to `29`
+  - ```
+    //        try {
+    //            Thread.sleep(100);
+    //        } catch (InterruptedException e) {
+    //            e.printStackTrace();
+    //        }
+    ```
---
+++ src/main/java/com/devlab/thread/miniproject/BankAccountSimulation.java
@@ -0,0 +1,111 @@
+package com.devlab.thread.miniproject;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
+class BankAccount {
+    private AtomicInteger balance = new AtomicInteger(1000);
+    ReentrantLock lock = new ReentrantLock();
+
+    public void deposit(int amount) {
+        try{
+            lock.lock();
+            balance.addAndGet(amount);
+            System.out.println("The person "+Thread.currentThread().getName()+" deposited amount: "+amount+" the balance is "+balance.get());
+            //Thread.sleep(100);
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void withDraw(int amount){
+//        try {
+//            Thread.sleep(100);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        if(lock.tryLock()) {
+            try {
+                if(balance.get() >= amount ) {
+                    balance.addAndGet(-amount);
+                    System.out.println("The person " + Thread.currentThread()
+                                                             .getName() + " withdraw amount: " + amount + " the balance is " + balance.get());
+                } else {
+                    System.out.println(Thread.currentThread().getName() + " tried to withdraw " + amount + " but insufficient balance.");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            System.out.println("Sorry, can not withdraw amount "+amount+" because other transaction is in progress..!!");
+        }
+    }
+
+    public int showBalance() {return balance.get();}
+
+}
+
+
+
+public class BankAccountSimulation {
+
+    public static void main(String[] args) throws InterruptedException {
+        BankAccount account = new BankAccount();
+        System.out.println("OPENNING BALANCE: "+account.showBalance());
+        Thread youtubeIncome = new Thread(() -> {
+            account.deposit(200);
+        },"youtubeIncome");
+
+        Thread freelancerIncome = new Thread(() -> {
+            account.deposit(500);
+        }, "freelancerIncome");
+
+        Thread cashDeposit = new Thread(() -> {
+            account.deposit(300);
+        },"cashDeposit");
+
+
+        Thread onlinePayment = new Thread(() -> {
+            account.withDraw(200);
+        },"onlinePayment");
+
+        Thread cashWithDraw = new Thread(() -> {
+            account.withDraw(500);
+        },"cashWithDraw");
+
+        Thread onlinePurchase = new Thread(() -> {
+            account.withDraw(300);
+        },"onlinePurchase");
+
+        // ------ deposit
+        youtubeIncome.start();
+        onlinePayment.start();
+
+        freelancerIncome.start();
+        cashWithDraw.start();
+
+        cashDeposit.start();
+        onlinePurchase.start();
+
+        // main should wait until all thread completed its work
+        // ------ deposit
+        youtubeIncome.join();
+        onlinePurchase.join();
+
+        onlinePayment.join();
+        freelancerIncome.join();
+
+        cashWithDraw.join();
+        cashDeposit.join();
+
+
+        Thread.sleep((long)(Math.random() * 200));
+        System.out.println("Closing BALANCE: "+account.showBalance());
+        System.out.println("Ending main thread");
+    }
+}
